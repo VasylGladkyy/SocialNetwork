6992c6aa9dde7019f305a23a58e9dc6fef8ec807
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-06-09 18:41:05 +0300

    add content for main page

--- src/main/java/com/site/social_network/service/MessageServiceImplement.java
+++ src/main/java/com/site/social_network/service/MessageServiceImplement.java
@@ -13,9 +13,9 @@ public class MessageServiceImplement implements MessageService {
 
     public List<Map<String,String>> messages= new ArrayList<Map<String, String>>(){{
         add(new HashMap<String, String>(){{put("id","1"); put("text","First message");}});
-        add(new HashMap<String, String>(){{put("id","2"); put("text","First message");}});
-        add(new HashMap<String, String>(){{put("id","3"); put("text","First message");}});
-        add(new HashMap<String, String>(){{put("id","4"); put("text","First message");}});
+        add(new HashMap<String, String>(){{put("id","2"); put("text","2 message");}});
+        add(new HashMap<String, String>(){{put("id","3"); put("text","3 message");}});
+        add(new HashMap<String, String>(){{put("id","4"); put("text","4 message");}});
     }};
 
     @Override
